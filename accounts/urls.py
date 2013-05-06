ca070be63edb772eaa239d2ace92f8f0b669160b
tsolar <tsolar@gmail.com>
2013-05-06 01:28:14 -0400

    accounts: added urls

---
+++ accounts/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import patterns, url
+
+
+urlpatterns = patterns('',
+                       
+                       url(r'^$', 'accounts.views.index', name='accounts_index'),
+                       url(r'^profile/', 'accounts.views.profile', name='accounts_profile'),
+                       
+)
