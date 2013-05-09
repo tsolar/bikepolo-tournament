bbb93648984fba80a101b718fb85178d43949ff8
tsolar <tsolar@gmail.com>
2013-05-09 02:59:25 -0400

    base.urls: administrar_membresia related urls

--- base/equipos_urls.py
+++ base/equipos_urls.py
@@ -8,4 +8,6 @@ urlpatterns = patterns('',
                        # url(r'^list/', EquiposListView.as_view(), name='base_equipos_list'),
                        url(r'^solicitar_membresia/$', 'base.views.equipos_solicitar_membresia', name='base_equipos_solicitar_membresia'),
                        url(r'^solicitar_membresia/(?P<equipo>[^/]+)/done$', 'base.views.equipos_solicitar_membresia_done', name='base_equipos_solicitar_membresia_done'),
+                       url(r'^administrar_membresia/$', 'base.views.equipos_administrar_membresia_index', name='base_equipos_administrar_membresia_index'),
+                       url(r'^administrar_membresia/(?P<equipo>[^/]+)/$', 'base.views.equipos_administrar_membresia_equipo', name='base_equipos_administrar_membresia_equipo'),
 )
