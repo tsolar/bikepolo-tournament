f791ef4e367c45b8a0a5b97968106a43d2e866e8
tsolar <tsolar@gmail.com>
2013-08-11 13:28:28 -0400

    base.equipos_urls: added borrar membresía

--- base/equipos_urls.py
+++ base/equipos_urls.py
@@ -13,5 +13,6 @@ urlpatterns = patterns('',
                        url(r'^membresia/admin/$', 'base.views.equipos_administrar_membresia_index', name='base_equipos_administrar_membresia_index'),
                        url(r'^membresia/admin/(?P<equipo_id>[^/]+)/$', 'base.views.equipos_administrar_membresia_equipo', name='base_equipos_administrar_membresia_equipo'),
                        url(r'^membresia/aprobar$', 'base.views.equipos_aprobar_membresia', name='base_equipos_aprobar_membresia'),
+                       url(r'^membresia/borrar/$', 'base.views.equipos_borrar_membresia', name='base_equipos_borrar_membresia'),
                        url(r'^lista_jugadores/$', 'base.views.equipos_get_lista_jugadores', name='base_equipos_get_lista_jugadores_equipo'),
 )
