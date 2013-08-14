fd64933e205415e1bac59cadb8819e7079822c2d
tsolar <tsolar@gmail.com>
2013-08-13 21:36:18 -0400

    accounts.urls: / is not used

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
                        
-                       url(r'^$', 'accounts.views.index', name='accounts_index'),
+                       # url(r'^$', 'accounts.views.index', name='accounts_index'),
                        url(r'^profile/', 'accounts.views.profile', name='accounts_profile'),
                        
 )
